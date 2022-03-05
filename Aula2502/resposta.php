70596037878f14c77f9e979df5bcd925ca3c3c1b
yelcodes <99371050+yelcodes@users.noreply.github.com>
2022-03-04 22:05:19 -0300

    Update resposta.php

--- Aula2502/resposta.php
+++ Aula2502/resposta.php
@@ -14,24 +14,39 @@
     <h1>Cálculos</h1>
     
     <?php
-        $valor1 = $_POST['valor1'];
-        $valor2 = $_POST['valor2']; 
-        echo "Valor 1 é: $valor1 e Valor 2 é: $valor2";
-        echo "<br/>";
-        echo "Soma: " ($valor1 + $valor2);
-        echo "Subtração: " ($valor1 - $valor2);
-        echo "Multiplicação: " ($valor1 * $valor2);
-       if ($valor2 !=0){
-           echo "Divisão: "($valor1 / $valor2);
-           }
-           else {
-               echo "Não é possível fazer a divisão por zero"
-           }
-        if ($valor1 % 2 == 0) {
-            echo "O valor 1 é par";
-          } else {
-            echo "O valor 1 é impar";
-          }
+      $valor1 = $_POST['valor1'];
+      $valor2 = $_POST['valor2'];
+      echo "Valor 1 é:  $valor1 e Valor 2 é:  $valor2";
+      echo "<br/>";
+      echo "Soma: ".($valor1 + $valor2);
+      echo "<br/>";
+      echo "Subtração: ".($valor1 - $valor2);
+      echo "<br/>";
+      echo "Multiplicação: ".($valor1 * $valor2);
+      echo "<br/>";
+      if ($valor2 != 0)
+         echo "Divisão: ".($valor1 / $valor2);
+      else 
+        echo "Não é possível realizar divisão por 0!";
+      if ($valor1 % 2 == 0)   
+        echo "Valor 1 é par!";
+      else
+        echo "Valor é impar";
+
+      $resultado = $valor1 % 2 == 0 ? "Valor 'é par!" : "Valor 1 é impar!";
+      echo $resultado;
+
+      $i = 10;
+
+      echo "<br/>";
+      
+      echo "Valor de i:".(3* $i++);
+
+      echo "<br/>";
+
+      echo "Valor de i:".($i)
+
+
     ?>
 
     <!-- Optional JavaScript; choose one of the two! -->
