5621ba5c1c4135585108fe95730975b2ebca0f5e
yelcodes <99371050+yelcodes@users.noreply.github.com>
2022-05-18 20:26:16 -0300

    1805

---
+++ ProjetoPHP/src/Model/DAO/ClientesDAO.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Aluno\ProjetoPhp\Model\DAO;
+
+use Aluno\ProjetoPhp\Model\Entity\Clientes;
+
+class ClientesDao{
+    
+    public function inserir(Clientes $c){
+        try{
+            $sql = "INSERT INTO `clientes`(`nome`, `email`, `idade`) VALUES (:nome, :email, :idade)";
+            
+        
+        } catch(\Exception $e){
+            return false;
+        }
+    }
+
+    public function alterar(Clientes $c){
+        try{
+            $sql = "UPDATE `clientes` SET `nome`=:nome,`email`=:email,`idade`=:idade WHERE 1";
+        }
+     
+        catch(\Exception $e){
+            return false;
+    }
+    }
+    
+    public function excluir($id){
+        try{
+            $sql = "DELETE FROM `clientes` WHERE id = :id";
+        }
+
+     catch(\Exception $e){
+         return false;
+    }
+    }
+
+    public function consultar(){
+     try{
+       $sql = "SELECT * FROM clientes";
+    }
+    catch(\Exception $e){
+        return false;
+    }
+    }
+    public function consultarPorId($id){
+        try{
+            $sql = "SELECT * FROM clientes WHERE id = :id";
+         }
+         catch(\Exception $e){
+             return false;
+         }
+    }
+}
