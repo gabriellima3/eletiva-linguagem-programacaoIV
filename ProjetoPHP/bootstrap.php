16fa51cb3b7bad97da7aa069b56b0232ed486003
yelcodes <gabriel.ferreira68@fatec.sp.gov.br>
2022-06-01 19:13:39 -0300

    .

--- ProjetoPHP/bootstrap.php
+++ ProjetoPHP/bootstrap.php
@@ -1,40 +1,53 @@
 <?php
 
-    require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/vendor/autoload.php';
 
-    //Aqui a gente recupera o que o usuário digitou e qual método HTPP ele utilizou
+//Aqui a gente recupera o que o usuário digitou e qual 
+//método HTTP ele utilizou
+$method = $_SERVER['REQUEST_METHOD'];
+$path = $_SERVER['PATH_INFO'];
 
-    $method = $_SERVER['REQUEST_METHOD'];
-    $path = $_SERVER["PATH_INFO"];
+//Instanciar classe Router
+$router = new \Aluno\ProjetoPhp\Router($method, $path);
 
-    //Instânciar classe Router
-    
-    $router = new \Aluno\ProjetoPhp\Router($method, $path);
+//ADICIONAR AS ROTAS VÁLIDAS ABAIXO
 
-    //Adicionar as rotas válidas abaixo
+$router->get('/ola-mundo', function(){
+    return "Olá Mundo!";
+});
 
-    $router->get('/ola-mundo',function(){
-        return "Hello World";
-    });
+$router->get('/exemplo', 
+        'Aluno\ProjetoPhp\Controller\ExercicioController::exibir');
 
-    $router->get('/exemplo', '\Aluno\ProjetoPhp\Controller\ExercicioController::exibir');
+$router->post('/exemplo-resultado',
+        'Aluno\ProjetoPhp\Controller\ExercicioController::exibirResultado');
 
-    $router->post('/exemplo/resultado','\Aluno\ProjetoPhp\Controller\ExercicioController::exibir');
+$router->get('/cliente/novo',
+'Aluno\ProjetoPhp\Controller\ClientesController::abrirFormularioInserir');
 
-    $router->get('/cliente/novo','Aluno\ProjetoPhp\Controller\ClientesController::abrirFormularioInserir');
+$router->post('/cliente/inserir',
+'Aluno\ProjetoPhp\Controller\ClientesController::inserirCliente');
 
-    $router->post('/cliente/inserir','\Aluno\ProjetoPhp\Controller\ClientesController::inserirCliente');
+$router->get('/clientes',
+'Aluno\ProjetoPhp\Controller\ClientesController::abrirListaClientes');
 
-    $router->get('/clientes','Aluno\ProjetoPhp\Controller\ClientesController::abrirListarClientes');
+$router->get('/cliente/alterar/{id}',
+'Aluno\ProjetoPhp\Controller\ClientesController::abrirFormularioAlterar');
 
-    //Adicionar as rotas válidas acima
+$router->post('/cliente/editar/{id}',
+'Aluno\ProjetoPhp\Controller\ClientesController::editarCliente');
 
-    $result = $router ->handler();
+$router->get('/cliente/excluir/{id}',
+'Aluno\ProjetoPhp\Controller\ClientesController::excluirCliente');
 
-    if (!$result){
-        http_response_code(404);
-        echo "Página não encontrada";
-        die();
-    }
+//ADICIONAR AS ROTAS VÁLIDAS ACIMA
 
-    echo $result($router->getParams());
+$result = $router->handler();
+
+if (!$result){
+    http_response_code(404);
+    echo "Página não encontrada";
+    die();
+}
+
+echo $result($router->getParams());
